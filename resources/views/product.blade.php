2d216375507aa2d4e1ed49a74eb0250bae4bb0a5
vrfgama <>
2023-08-27 19:34:17 -0300

    view product correction, quantity implemented

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,24 +3,32 @@
 
 @section('content')
 
-{{ $product->name }}
-<br>
-{{ $product->description }}
-<br>
-R$
-{{ $product->price }}
-<br>
-Quantidade:
-<br>
-<select>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-</select>
 
-<br><br>
-<a href="{{ route('cart.add', $product->id) }}">Adicionar ao carrinho</a>
+<form action="{{ route('cart.add', $product->id ) }}" method="post">
+        @csrf
+        <br>
+        {{ $product->name }}
+        <br>
+        {{ $product->description }}
+        <br>
+        R$ {{ $product->price }}
+        
+        <br>
+
+        Quantidade:
+        <br>
+        <select name="qtd" id="qtd">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <br><br>
+
+        <input type="submit" value="Adicionar ao carrinho">
+        <br>
+
+        </form>
 
 @endsection
